d8ab54f5812bb2cbc6f7d5298657c81e7691dc9c
cissica <cecemartinez@hotmail.com>
2021-03-24 22:32:03 -0700

    adding password_digest and updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_051136) do
+ActiveRecord::Schema.define(version: 2021_03_25_053033) do
 
   create_table "notes", force: :cascade do |t|
     t.string "content"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2021_03_24_051136) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "email"
   end
 
 end
