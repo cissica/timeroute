97f13c5b2cc746020937e04c0246cefcc8249725
cissica <cecemartinez@hotmail.com>
2021-03-24 23:17:25 -0700

    adding to controllers and adding corresponding views

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,19 @@
 class SchedulesController < ApplicationController
+    def new
+        @schedule = Schedule.new
+    end 
+
+    def create 
+        @schedule = Schedule.new(schedule_params)
+        if @schedule.save
+            redirect_to schedules_path
+        else 
+            render 'schedules/new.html.erb'
+            #with error messages
+        end 
+    end 
+
+    def index
+        
+    end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,14 @@ class UsersController < ApplicationController
     end 
 
     def create
-        @user = User.new(post_params)
-        @user.save
-        redirect_to @user
+        @user = User.new(user_params)
+        if @user.save 
+            session[:user_id] = @user.id
+                redirect_to new_schedule_path
+        else
+            render 'users/new.html.erb'
+            #render signup form again with error messages 
+        end 
     end 
 
     def show
@@ -15,7 +20,7 @@ class UsersController < ApplicationController
     end 
 
     private 
-    def post_params
-        params.require(:user).permit(:name)
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
     end
 end
