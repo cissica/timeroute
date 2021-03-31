fae84f84050f3332de28c6d1486b9e7f4fd66aaa
cissica <cecemartinez@hotmail.com>
2021-03-30 23:04:55 -0700

    adding time option to task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -36,7 +36,7 @@ class TasksController < ApplicationController
 
     private 
     def task_params
-        params.require(:task).permit(:description, :schedule_id)
+        params.require(:task).permit(:task_title, :description, :schedule_id, :when)
     end 
 
 end
