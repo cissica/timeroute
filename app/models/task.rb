7388819e9d24bcb9aa8ab23647cf230d1964dfa4
cissica <cecemartinez@hotmail.com>
2021-03-31 05:57:42 -0700

    adjusting time

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
     belongs_to :schedule 
     has_many :notes
-    validates :task_title, :description, :when, presence: true   
+    validates :task_title, :description, :when, presence: true  
+    
+    scope :display_by_when, -> { order(:when) }
 end
